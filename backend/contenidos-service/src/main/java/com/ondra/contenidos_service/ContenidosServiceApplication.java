e731be9e960f6ffc0215f753c4d149243f341f27
Miguel Ángel <miguelitoci16@gmail.com>
2025-10-25 21:37:52 +0200

    Inicializar microservicio de Contenidos con Spring Boot: estructura base, configuración PostgreSQL y dependencias iniciales

---
+++ backend/contenidos-service/src/main/java/com/ondra/contenidos_service/ContenidosServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ondra.contenidos_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContenidosServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContenidosServiceApplication.class, args);
+	}
+
+}
